2b13231baff49e20409b5195ffc299233e530144
Dávid Horváth <horvath@webarticum.hu>
2025-01-12 01:23:37 +0100

    Add rm to docker start scripts

--- examples/holodb-standalone/start.sh
+++ examples/holodb-standalone/start.sh
@@ -5,4 +5,5 @@ set -e
 selfDir="$( dirname -- "$( realpath "$0" )" )"
 name="$( cat "${selfDir}/name.txt" )"
 
+docker rm -f "${name}" > /dev/null 2>&1
 docker run --name="${name}" -p 3430:3430 -d miniconnect/"${name}":latest
