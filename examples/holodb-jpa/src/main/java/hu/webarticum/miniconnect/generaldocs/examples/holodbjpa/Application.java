d26cea9eb72176d2f6ba37eb4880b23c5610dda9
Dávid Horváth <horvath@webarticum.hu>
2026-01-06 09:09:43 +0100

    Remove event specific mentions from the jpa demo

--- examples/holodb-jpa/src/main/java/hu/webarticum/miniconnect/generaldocs/examples/holodbjpa/Application.java
+++ examples/holodb-jpa/src/main/java/hu/webarticum/miniconnect/generaldocs/examples/holodbjpa/Application.java
@@ -4,7 +4,7 @@ import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@OpenAPIDefinition( info = @Info( title = "REST API Demo", description = "REST API Demo powered by HoloDB (InnOtdk 2025)" ) )
+@OpenAPIDefinition( info = @Info( title = "REST API Demo", description = "REST API Demo powered by HoloDB" ) )
 public class Application {
 
     public static void main(String[] args) {
